FRiCKLE/ngx_postgres

commit 1bc3b86cd852278a974b46b11baf31847eb6a8f8

Remove int_strlen(), it should be gone long time ago, because it doesn't follow nginx's coding practices.

src/ngx_postgres_upstream.c
@@ -226,19 +226,6 @@ failed:
 #endif
 }
 
-size_t
-int_strlen(ngx_uint_t n)
-{
-    size_t s = 1;
-
-    while (n >= 10) {
-        n /= 10;
-        s++;
-    }
-
-    return s;
-}
-
 ngx_int_t
 ngx_postgres_upstream_get_peer(ngx_peer_connection_t *pc, void *data)
 {
@@ -332,7 +319,7 @@ ngx_postgres_upstream_get_peer(ngx_peer_connection_t *pc, void *data)
 
     /* sizeof("...") - 1 + 1 (for spaces and '\0' omitted */
     len = sizeof("hostaddr=") + peer->host.len
-        + sizeof("port=") + int_strlen(peer->port)
+        + sizeof("port=") + sizeof("65535") - 1
         + sizeof("dbname=") + peer->dbname.len
         + sizeof("user=") + peer->user.len
         + sizeof("password=") + peer->password.len

commit 0e4fa7fb0450db64e38e4060396944f5b041f676

Fix off-by-few error that could lead to failed connection to the database.
This bug was introduced in revision 1bc3b86cd852278a974b46b11baf31847eb6a8f8 and it was catched by unfriendly environment of the build farm for nginx modules (yay!).

src/ngx_postgres_upstream.c
@@ -240,7 +240,7 @@ ngx_postgres_upstream_get_peer(ngx_peer_connection_t *pc, void *data)
     int                                 fd;
     ngx_event_t                        *rev, *wev;
     ngx_int_t                           rc;
-    u_char                             *connstring;
+    u_char                             *connstring, *last;
     size_t                              len;
 
     dd("entering");
@@ -336,13 +336,12 @@ ngx_postgres_upstream_get_peer(ngx_peer_connection_t *pc, void *data)
     }
 
     /* TODO add unix sockets */
-    (void) ngx_snprintf(connstring, len - 1,
+    last = ngx_snprintf(connstring, len - 1,
                         "hostaddr=%V port=%d dbname=%V user=%V password=%V"
                         " sslmode=disable",
                         &peer->host, peer->port, &peer->dbname, &peer->user,
                         &peer->password);
-
-    connstring[len - 1] = '\0';
+    *last = '\0';
 
     dd("PostgreSQL connection string: %s", connstring);
 