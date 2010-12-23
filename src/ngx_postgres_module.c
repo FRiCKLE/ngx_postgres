8d3a62fec3915e3c77570d637e741b46e11df18e
Piotr Sikora <piotr.sikora@frickle.com>
2010-12-23 02:39:52 +0000

    Revert fixed jump on uninitialized value.
    The problem is that this value shouldn't be used in the first place
    (see next commit).
    
    This reverts commit 1ed9f15f2becee512a7f0608d39cb4f9e4890f26.

--- src/ngx_postgres_module.c
+++ src/ngx_postgres_module.c
@@ -1069,8 +1069,6 @@ ngx_postgres_conf_output(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
                            value[3].data, value[3].len + 1);
     }
 
-    pglcf->output_value->required = 0;
-
     dd("returning NGX_CONF_OK");
     return NGX_CONF_OK;
 }
