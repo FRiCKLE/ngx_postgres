7bd32c821efdaf38ada95d576762a37ea253525c
agentzh (章亦春) <agentzh@gmail.com>
2011-09-25 12:48:41 +0800

    fixed compatibility issues with nginx 1.1.4: ngx_chain_update_chains now requires a pool argument.

--- src/ngx_postgres_output.c
+++ src/ngx_postgres_output.c
@@ -30,6 +30,7 @@
 #include "ngx_postgres_ddebug.h"
 #include "ngx_postgres_module.h"
 #include "ngx_postgres_output.h"
+#include <nginx.h>
 
 
 ngx_int_t
@@ -580,7 +581,11 @@ ngx_postgres_output_chain(ngx_http_request_t *r, ngx_chain_t *cl)
         return rc;
     }
 
+#if defined(nginx_version) && nginx_version >= 1001004
+    ngx_chain_update_chains(r->pool, &u->free_bufs, &u->busy_bufs, &cl, u->output.tag);
+#else
     ngx_chain_update_chains(&u->free_bufs, &u->busy_bufs, &cl, u->output.tag);
+#endif
 
     dd("returning rc:%d", (int) rc);
     return rc;
